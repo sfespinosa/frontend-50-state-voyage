e32a4352eac36805e5beebae99efd904ee306331
sfespinosa <scottjames.espinosa@gmail.com>
2020-09-02 15:38:03 -0700

    popover image

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -6,6 +6,7 @@ const MapMarker = ({establishment, category}) => {
         <>
         <i class="material-icons md-dark md-36 info600" id={establishment.reference_id}>room</i>{' '}
             <UncontrolledTooltip placement='top' target={establishment.reference_id} delay={0}>
+                <img src={establishment.img_url} className='popover-image' alt='establishment thumbnail'/><br/>
                 {establishment.name}<br/>
                 {establishment.address}<br/>
                 Category: {category ? category : 'n/a'}
